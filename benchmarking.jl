75f6dde9f376972afb9164c2ae0015b8b74660d7
lijas <elias.borjesson@hotmail.com>
2019-02-19 21:34:29 +0100

    benchmark pid-functions

--- benchmarking.jl
+++ benchmarking.jl
@@ -16,3 +16,9 @@ reset!(game)
     ego_search(game, e0.mm, visited)
 end
 
+#Check how long a game between Montecarlo players take
+#They should always be picking the same move...
+reset!(game)
+p1 = MonteCarloPlayer(10)
+p2 = MonteCarloPlayer(10)
+@benchmark pit(game,p1,p2; should_print = false)
