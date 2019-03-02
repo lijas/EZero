e331767263d92feec79d391fd38850d03b6fe90b
lijas <elias.borjesson@hotmail.com>
2019-03-02 09:30:30 +0100

    more benchmarks

--- benchmarking.jl
+++ benchmarking.jl
@@ -4,25 +4,41 @@ include("src/EZero.jl")
 game = Connect4()
 move = Connect4Move(4)
 
-@benchmark begin
+@btime begin
     make_move!(game, move)
     take_move!(game, move)
 end
 
-e0 = EGo()
+e0 = EZero1()
 reset!(game)
-@benchmark begin 
+@btime begin 
     visited = Dict{Int, Vector{Any}}()
-    ego_search(game, e0.mm, visited)
+    reset!(game)
+    for _ in 1:50
+        ego_search(game, e0.mm, visited)
+    end
 end
 
 #Check how long a game between Montecarlo players take
 #There are some randomness to montecarlo, so probobly
 #not the best way to benchmark
+#=
 reset!(game)
 p1 = MonteCarloPlayer(100)
 p2 = MonteCarloPlayer(10)
-@benchmark begin
+@btime begin
     reset!(game)
     pit(game,p1,p2; should_print = false)
 end
+=#
+
+reset!(game)
+make_move!(game, Connect4Move(4));make_move!(game, Connect4Move(5))
+make_move!(game, Connect4Move(4));make_move!(game, Connect4Move(4))
+make_move!(game, Connect4Move(3));make_move!(game, Connect4Move(1))
+make_move!(game, Connect4Move(6));make_move!(game, Connect4Move(4))
+make_move!(game, Connect4Move(3));make_move!(game, Connect4Move(7))
+make_move!(game, Connect4Move(3));make_move!(game, Connect4Move(2))
+@btime was_last_move_a_win(game)
+make_move!(game, Connect4Move(1));make_move!(game, Connect4Move(3))
+@btime was_last_move_a_win(game);
